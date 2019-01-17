c6356da99ebeba63cc13c86b2fe41a30979a9f14
hu_pf <hu_pf@suixingpay.com>
2019-01-17 19:55:04 +0800

    多项目

---
+++ rpc-client/src/main/java/com/example/rpcclient/RpcClientApplication.java
@@ -0,0 +1,14 @@
+package com.example.rpcclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcClientApplication.class, args);
+    }
+
+}
+
---
+++ rpc-server/src/main/java/com/example/rpcserver/RpcServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.rpcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpcServerApplication.class, args);
+	}
+
+}
+
