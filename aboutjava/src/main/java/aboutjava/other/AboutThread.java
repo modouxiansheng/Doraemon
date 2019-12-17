5c145b1fd3821d99f4f3b40319cea1145b05aeb3
“hu_pf” <hu_pf@suixingpay.com>
2019-12-17 17:26:07 +0800

    如何优雅的使用线程池

--- aboutjava/src/main/java/aboutjava/other/AboutThread.java
+++ aboutjava/src/main/java/aboutjava/other/AboutThread.java
@@ -1,9 +1,7 @@
 package aboutjava.other;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @program: springBootPractice
@@ -14,17 +12,97 @@ import java.util.concurrent.TimeUnit;
 public class AboutThread {
 
     public static void main(String[] args) {
+        // 线程池饥饿死锁
+//        ThreadDeadLock threadDeadLock = new ThreadDeadLock();
+//        threadDeadLock.threadDeadLock();
+
+        customerThread();
+
+    }
+
+    public static void customerThread(){
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(0,Integer.MAX_VALUE,1, TimeUnit.SECONDS,new SynchronousQueue<>(),
+                new CustomerThreadFactory("customerThread"));
+
+        for (int i = 0; i < 10; i++) {
+            threadPoolExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.printf(Thread.currentThread().getName());
+                    System.out.printf("\n");
+                }
+            });
+        }
+    }
+
+
+    public static void initThread(){
         Executors.newFixedThreadPool(10);
         Executors.newSingleThreadExecutor();
 
         Executors.newCachedThreadPool();
         Executors.newScheduledThreadPool(10);
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1,1,1, TimeUnit.SECONDS,new LinkedBlockingQueue<>());
-        threadPoolExecutor.execute(new Runnable() {
-            @Override
-            public void run() {
-                System.out.printf("1");
-            }
-        });
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(0,Integer.MAX_VALUE,1, TimeUnit.SECONDS,new SynchronousQueue<>());
+        for (int i = 0; i < 10; i++) {
+            threadPoolExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.printf("1");
+                    System.out.printf("\n");
+                }
+            });
+        }
     }
 }
+
+class ThreadDeadLock{
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    public void threadDeadLock(){
+        Future<String> taskOne  = executorService.submit(new TaskOne());
+        try {
+            System.out.printf(taskOne.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class TaskOne implements Callable{
+
+        @Override
+        public Object call() throws Exception {
+            Future<String> taskTow = executorService.submit(new TaskTwo());
+            return "TaskOne" + taskTow.get();
+        }
+    }
+
+    public class TaskTwo implements Callable{
+
+        @Override
+        public Object call() throws Exception {
+            return "TaskTwo";
+        }
+    }
+}
+
+class CustomerThreadFactory implements ThreadFactory{
+
+    private String name;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    CustomerThreadFactory(String name){
+        this.name = name;
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread thread = new Thread(r,name+threadNumber.getAndIncrement());
+        return thread;
+    }
+}
+
