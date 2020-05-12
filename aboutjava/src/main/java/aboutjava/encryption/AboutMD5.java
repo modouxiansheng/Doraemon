9243dc2f3ec09de6074eb89e160e51556b7d1b60
D.Hu <405497511@qq.com>
2020-05-12 18:29:36 +0800

    关于加解密、加签验签的那些事

---
+++ aboutjava/src/main/java/aboutjava/encryption/AboutMD5.java
@@ -0,0 +1,38 @@
+package aboutjava.encryption;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @program: springBootPractice
+ * @description:
+ * @author: hu_pf
+ * @create: 2020-05-12 17:44
+ **/
+public class AboutMD5 {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(getMD5Str("xxxxx"));
+    }
+
+
+    public static String getMD5Str(String str) throws Exception {
+        try {
+            // 生成一个MD5加密计算摘要
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            // 计算md5函数
+            md.update(str.getBytes());
+            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
+            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
+            return new BigInteger(1, md.digest()).toString(16);
+        } catch (Exception e) {
+            throw new Exception("MD5加密出现错误，"+e.toString());
+        }
+    }
+}
--- aboutjava/src/main/java/aboutjava/encryption/AboutRsa.java
+++ aboutjava/src/main/java/aboutjava/encryption/AboutRsa.java
@@ -1,5 +1,15 @@
 package aboutjava.encryption;
 
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.security.*;
+import java.util.Base64;
+import java.util.Map;
+
 /**
  * @program: springBootPractice
  * @description: 关于RSA的非对称加密算法
@@ -7,8 +17,76 @@ package aboutjava.encryption;
  * @create: 2020-05-08 17:27
  **/
 public class AboutRsa {
+    public static final String RSA_ALGORITHM = "RSA";
 
-    public static void main(String[] args) {
+    public static final Charset UTF8 = Charset.forName("UTF-8");
 
+    public static void main(String[] args) throws Exception {
+        KeyPair keyPair = buildKeyPair();
+
+        byte[] encryptData = encrypt(keyPair.getPublic(), "不学无数");
+
+        System.out.println(String.format("加密后的数据：%s",base64Encode(encryptData)));
+
+        System.out.println(String.format("解密后的数据：%s",new String(decrypt(keyPair.getPrivate(),encryptData),UTF8)));
+
+        String context = "加签的字符串";
+
+        String sign = signWithRSA(context, keyPair.getPrivate());
+
+        System.out.println(String.format("生成的签名：%s",sign));
+
+        Boolean checkSignWithRSA = checkSignWithRSA(context, keyPair.getPublic(), sign);
+
+        System.out.println(String.format("校验的结果：%s",checkSignWithRSA.toString()));
+    }
+
+    public static KeyPair buildKeyPair() throws NoSuchAlgorithmException {
+        final int keySize = 2048;
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(RSA_ALGORITHM);
+        keyPairGenerator.initialize(keySize);
+        return keyPairGenerator.genKeyPair();
+    }
+
+    public static byte[] decrypt(PrivateKey privateKey, byte [] encrypted) throws Exception {
+        Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+        return cipher.doFinal(encrypted);
+    }
+
+    public static byte[] encrypt(PublicKey publicKey, String message) throws Exception {
+        Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        return cipher.doFinal(message.getBytes(UTF8));
+    }
+
+    public static String base64Encode(byte[] data) {
+        return new BASE64Encoder().encode(data);
+    }
+    public static byte[] base64Decode(String data) throws IOException {
+        return new BASE64Decoder().decodeBuffer(data);
+    }
+
+    /**
+     * 使用RSA签名
+     */
+    private static String signWithRSA(String content, PrivateKey privateKey) throws Exception {
+        Signature signature = Signature.getInstance("SHA1WithRSA");
+        signature.initSign(privateKey);
+        signature.update(content.getBytes("utf-8"));
+        byte[] signed = signature.sign();
+        return base64Encode(signed);
+    }
+
+    /**
+     * 使用RSA验签
+     */
+    private static boolean checkSignWithRSA(String content, PublicKey publicKey,String sign) throws Exception {
+        Signature signature = Signature.getInstance("SHA1WithRSA");
+        signature.initVerify(publicKey);
+        signature.update(content.getBytes("utf-8"));
+        return signature.verify(base64Decode(sign));
     }
 }
